93c797beaac7b39a4f64d0b3c27295e204f647d6
imane sbiti <emysb2020@gmail.com>
2023-05-24 06:50:07 -0400

    first commit

---
+++ src/assets/components/MyFirstCompo.jsx
@@ -0,0 +1,11 @@
+// Il est fortemenet recommendé dans le cours de nommer le nom de la fonction comme le nom du fichier JSX
+// faire le racourci rafce pour créer la fonction automatiquement
+const MyFirstCompo = () => {
+    return(
+        <section>
+            <h1>Salut c'est moi je suis ici</h1>
+        </section>
+    );
+};
+
+export default MyFirstCompo;
